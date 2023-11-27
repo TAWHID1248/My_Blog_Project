287ac3bc53b960616fd44f18c37ad50a74cde7a6
tawhid1248 <frzfardin2020@gmail.com>
2023-11-27 16:39:31 +0600

    like/unlike half

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Dell XPS\Desktop\dj-Bohubrihi\My_Blog_Project\venv
+set VIRTUAL_ENV=C:\Users\User\Desktop\Bohubrihi\My_Blog_Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
